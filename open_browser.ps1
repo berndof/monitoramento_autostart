70760efff9ac27b0a996040ccbc8a66af62c5352
berndof <bernasoestudas@gmail.com>
2025-07-16 10:09:50 -0300

    change grafana link

--- open_browser.ps1
+++ open_browser.ps1
@@ -4,4 +4,4 @@ $edge = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 Start-Process $edge -ArgumentList "--app=https://monitoramento.scc.com.br/zabbix.php?action=dashboard.view --start-fullscreen"
 
 
-Start-Process $edge -ArgumentList "--app=https://rotadigital.scc.com.br/d/fejvra6ovbbwga/ti-lages-links?orgId=1&from=now-1h&to=now&timezone=browser&refresh=1m"
+Start-Process $edge -ArgumentList "--app=https://rotadigital.scc.com.br/playlists/play/deojo11yk8mioc?kiosk"
